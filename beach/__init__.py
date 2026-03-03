8795b63c2051792d4db5076f3252cbe086c2049f
Jin Nakazono <71783375+Nkzono99@users.noreply.github.com>
2026-03-03 23:22:06 +0900

    Remove legacy Python simulation modules

--- beach/__init__.py
+++ beach/__init__.py
@@ -1,66 +1,14 @@
 """BEACH (BEM + Accumulated CHarge) Python package."""
 
-from .sim import (
-    BEMElement,
-    BEMMesh,
-    Particle,
-    SimConfig,
-    TestParticleSimulator,
-    calc_electric_field,
-    BEMField,
-    ZeroB,
-    UniformB,
-    AbsorptionInteraction,
-    InsulatorSurfaceCharge,
-)
-from .geometry import (
-    make_plane,
-    make_box,
-    make_cylinder,
-    make_sphere,
-    mesh_from_vertices_faces,
-    merge_meshes,
-)
-from .importers import MeshImporter, OBJImporter, FreeCADImporter, load_mesh
-from .fortran_results import FortranRunResult, load_fortran_result, list_fortran_runs, plot_charge_mesh, plot_charges
-
-from .injection import (
-    PositionSampler,
-    VelocitySampler,
-    UniformPositionSampler,
-    ShiftedMaxwellVelocitySampler,
-    RandomBeamInjector,
-    FixedBeamInjector,
+from .fortran_results import (
+    FortranRunResult,
+    list_fortran_runs,
+    load_fortran_result,
+    plot_charge_mesh,
+    plot_charges,
 )
 
 __all__ = [
-    "BEMElement",
-    "BEMMesh",
-    "Particle",
-    "SimConfig",
-    "TestParticleSimulator",
-    "calc_electric_field",
-    "BEMField",
-    "ZeroB",
-    "UniformB",
-    "AbsorptionInteraction",
-    "InsulatorSurfaceCharge",
-    "make_plane",
-    "make_box",
-    "make_cylinder",
-    "make_sphere",
-    "mesh_from_vertices_faces",
-    "merge_meshes",
-    "MeshImporter",
-    "OBJImporter",
-    "FreeCADImporter",
-    "load_mesh",
-    "PositionSampler",
-    "VelocitySampler",
-    "UniformPositionSampler",
-    "ShiftedMaxwellVelocitySampler",
-    "RandomBeamInjector",
-    "FixedBeamInjector",
     "FortranRunResult",
     "load_fortran_result",
     "list_fortran_runs",
--- beach/sim/constants.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-import numpy as np
-
-EPS0 = 8.8541878128e-12
-K_COULOMB = 1.0 / (4.0 * np.pi * EPS0)
